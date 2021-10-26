7f8192a0dbbd7e7fc5e9400e47ed5efb055634af
Ferenc <o7400293275@gmail.com>
2021-10-26 15:23:11 +0200

    UrlFixer exercises added

---
+++ week-03/src/UrlFixer.java
@@ -0,0 +1,19 @@
+public class UrlFixer {
+    public static void main(String[] args) {
+        String url = "https//www.reddit.com/r/nevertellmethebots";
+
+        // Véletlenül rossz URL-t kaptam egy vicces subreddithez.
+        // Valószínűleg ez "odds" és nem "bots" akart lenni.
+        // Továbbá az URL-ből hiányzik egy fontos rész, derítsd ki
+        // melyik az és illeszd be.
+        // Próbáld meg megoldani többféleképpen különböző String függvényekkel.
+
+
+
+        url = url.replace("https", "https:");
+        url = url.replace("bots", "odds");
+
+        System.out.println(url);
+    }
+
+}
